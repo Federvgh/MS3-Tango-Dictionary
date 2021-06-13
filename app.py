5baaa82be492507ecd5413aec64ec405c0fb8aae
Federvgh <caffarofederico@gmail.com>
2021-06-13 19:45:52 +0000

    template add_word.html added

--- app.py
+++ app.py
@@ -107,6 +107,12 @@ def logout():
     return redirect(url_for("login"))   
 
 
+@app.route("/add_word")
+def add_word():
+    return render_template("add_word.html")
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
